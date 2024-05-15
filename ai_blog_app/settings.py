1f6c673e80c513a4c4bf632d01c092d0905e1d59
Sonukumar <sonumandal048@gmail.com>
2024-05-15 19:46:59 +0530

    fixed login error and updated logout, index method

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ai_blog_app.urls'
+LOGIN_URL = 'login'
 
 TEMPLATES = [
     {
@@ -74,22 +75,33 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
+#         'USER' :'',
+#         'PASSWORD' : '',
+#         'HOST' :'' ,
+#         'PORT': ''
+
 #     }
 # }
 
+#do not remove this one vishal , just commnet out it on your env
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER' :'',
-        'PASSWORD' : '',
-        'HOST' :'' ,
-        'PORT': ''
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'OPTIONS': {
+        #     'options': '-c search_path=wsmaster0036'
+        # },
+        'HOST': '127.0.0.1',
+        'NAME': 'AI_blog_G',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'PORT': 5432,
+        'DISABLE_SERVER_SIDE_CURSORS': True,
+        'CONN_MAX_AGE': None,
     }
 }
 
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,53 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
-
-# Create your views here.
+@login_required
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
-        user = authenticate(request,username = username , password=password)
-        if user is not  None:
-            login(request,user)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
             return redirect('/')
         else:
-            error_message = 'Ivalid Username or password'
-            return render(request,'login.html',{'error_message' : error_message})        
-    return render(request,'login.html')
+            error_message = 'Invalid Username or Password'
+            return render(request, 'login.html', {'error_message': error_message})
+    
+    return render(request, 'login.html')
 
 def user_signup(request):
     if request.method == 'POST':
         username = request.POST['username']
-        email = request.POST['Email']
+        email = request.POST['email']
         password = request.POST['password']
-        repeatPassword = request.POST['repeatPassword']   
+        repeat_password = request.POST['repeatPassword']
 
-        if password == repeatPassword:
+        if password == repeat_password:
             try:
-                user = User.objects.create_user(username,email,password)
-                user.save()
-                login(request,user)
+                user = User.objects.create_user(username, email=email, password=password)
+                login(request, user)
                 return redirect('/')
-
-            except:
-                error_message = 'Error Creating account' 
-
-
+            except IntegrityError:
+                error_message = 'Username or Email already exists.'
         else:
-            error_message = 'Password do not match '
-            return render ( request,'signup.html' ,{'error_message': error_message})
-            return render (request,'signup.html')
+            error_message = 'Passwords do not match.'
 
+        return render(request, 'signup.html', {'error_message': error_message})
 
-
-    return render (request,'signup.html')
+    return render(request, 'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('login')
